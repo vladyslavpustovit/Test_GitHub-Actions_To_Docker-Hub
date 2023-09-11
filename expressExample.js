c8b35b162b428bc4fa9b71819fff3819a558c8c2
vladyslavpustovit <Vladyslav.Pustovit@student.intecbrussel.be>
2023-09-11 20:14:04 +0200

    Simple API for testing purpose

---
+++ expressExample.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const router = express.Router();
+const app = express();
+require('dotenv').config();
+
+app.use(cors({
+    origin: '*'
+}))
+
+app.use(express.json());
+
+app.use("/", router);
+
+router.get("/api", (req, res)=>{
+    res.json({ message: "GET REQUEST SUCCESS" });
+});
+
+// POST route for testing
+router.post("/api", (req, res) => {
+    const requestData = req.body;
+    // You can do something with the posted data here
+    res.json({ message: "POST REQUEST SUCCESS.", data: requestData });
+});
+
+const PORT = 1337;
+app.listen(PORT, (err) => {
+    err ? console.log(err) : console.log(`Listening port ${PORT}`);
+});
